cb030806813c50bba1134f92052d876b89333ad5
shyneii <90271556+shyneii@users.noreply.github.com>
2023-01-16 18:57:30 -0500

    Initial commit

---
+++ ProgramemesCards.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using ProgramemesCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace ProgramemesCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class MyModName : BaseUnityPlugin
+    {
+        private const string ModId = "com.My.Mod.Id";
+        private const string ModName = "MyModName";
+        public const string Version = "0.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "PC";
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            CustomCard.BuildCard<MyCardName>();
+        }
+        public static ProgramemesCards instance { get; private set; }
+
+        
+    }
+}
+
+
+
